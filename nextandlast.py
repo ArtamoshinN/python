94e7c1fada85e3a83ef6e43ecaebbc4ca2e4ea25
Никита <nikita@MacBook-Pro-Markovic-2.local>
2018-02-27 01:14:46 +0300

    first week

---
+++ nextandlast.py
@@ -0,0 +1,5 @@
+n = int(input())
+n1 = n + 1
+n2 = n - 1
+print('The next number for the number ', n, ' is ', n1, '.', sep='')
+print('The previous number for the number ', n, ' is ', n2, '.', sep='')
---
+++ oneorzero.py
@@ -0,0 +1,3 @@
+a = int(input())
+a = 0 ** a
+print(a)
